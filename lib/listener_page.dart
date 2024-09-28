3d02d58c24d6fea663bd1e8e8c60d537a63b5bfd
eveil <erick@erickveil.net>
2024-09-28 15:28:43 -0700

    Attempting to center the contents in the window

--- lib/listener_page.dart
+++ lib/listener_page.dart
@@ -11,75 +11,80 @@ class ListenerPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Socket Listener'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Consumer<ListenerViewModel>(
-          builder: (context, viewModel, child) {
-            return Column(
-              // minimize the vertical space use
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: _portController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: 'Port Number',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                Row(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_portController.text.isNotEmpty) {
-                          int port = int.parse(_portController.text);
-                          viewModel.toggleListener(port);
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 162, 210, 234)
-                      ),
-                      child: Text(viewModel.isListening 
-                        ? 'Stop Listener' 
-                        : 'Start Listener'
-                        )
-                    ),
-                    SizedBox(width: 16.0),
-                    Text(
-                      viewModel.isListening ? 'Listening...' : 'Stopped',
-                      style: TextStyle(
-                        color: viewModel.isListening 
-                          ? const Color.fromARGB(255, 46, 107, 48) 
-                          : const Color.fromARGB(255, 112, 29, 23),
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 24.0),
-                Expanded(
-                  child: Container(
+      body: Center ( 
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Consumer<ListenerViewModel>(
+            builder: (context, viewModel, child) {
+              return Column(
+                // minimize the vertical space use
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
                     width: 300,
-                    height: 150,
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xffe6e8fa),
-                      border: Border.all(color: Colors.blueGrey),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: SingleChildScrollView(
-                      child: Text(
-                        viewModel.receivedMessages,
-                        style: TextStyle(fontSize: 16.0),
+                    child: TextField(
+                      controller: _portController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Port Number',
+                        border: OutlineInputBorder(),
                       ),
                     ),
                   ),
-                ),
-              ],
-            );
-          },
+                  SizedBox(height: 16.0),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_portController.text.isNotEmpty) {
+                            int port = int.parse(_portController.text);
+                            viewModel.toggleListener(port);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 162, 210, 234)
+                        ),
+                        child: Text(viewModel.isListening 
+                          ? 'Stop Listener' 
+                          : 'Start Listener'
+                          )
+                      ),
+                      SizedBox(width: 16.0),
+                      Text(
+                        viewModel.isListening ? 'Listening...' : 'Stopped',
+                        style: TextStyle(
+                          color: viewModel.isListening 
+                            ? const Color.fromARGB(255, 46, 107, 48) 
+                            : const Color.fromARGB(255, 112, 29, 23),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24.0),
+                  Expanded(
+                    child: Container(
+                      width: 300,
+                      height: 150,
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xffe6e8fa),
+                        border: Border.all(color: Colors.blueGrey),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          viewModel.receivedMessages,
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
