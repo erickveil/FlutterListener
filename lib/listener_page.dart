934b1045e44acfaf5a6e00b6e639f8a1130b558d
eveil <erick@erickveil.net>
2024-10-03 16:27:07 -0700

    Full page refactor

--- lib/listener_page.dart
+++ lib/listener_page.dart
@@ -10,6 +10,30 @@ class ListenerPage extends StatelessWidget {
   final TextEditingController _portController = 
     TextEditingController(text: '50502');
 
+  @override
+  Widget build(BuildContext context) {
+
+    Consumer pageBuilder = Consumer<ListenerViewModel>( 
+      builder: _buildListenerPage
+    );
+
+    // Keep elements inside the safe area on mobile.
+    Padding edgeSaver = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: pageBuilder,
+    );
+
+    Container pageRoot = Container(
+      width: 350,
+      child: edgeSaver,
+    );
+
+    Center pageAlignement = Center ( child: pageRoot );
+    
+    // Scaffold is a basic application container that provides a lot of basic
+    // Application stuff in its members for free, like an AppBarr or a body.
+    return Scaffold( body: pageAlignement);
+  }
 
   Container createPortInput() {
     InputDecoration portInputHelperText = InputDecoration(
@@ -99,68 +123,33 @@ class ListenerPage extends StatelessWidget {
     return Expanded(child: outputBox);
   }
 
-  @override
-  Widget build(BuildContext context) {
+  /// context - the current build context, lets us access other widgets or data 
+  ///   from the widget tree. We don't really make use of that here.
+  /// viewModel - an instance of ListenerViewModel - you'll see me use this to 
+  ///   check the state before I do things here like set the text and color on 
+  ///   the connection indicator.
+  /// child - a widget that doesn't need to be rebuilt (unused).
+  Widget _buildListenerPage(
+    BuildContext context, ListenerViewModel viewModel, Widget? child) {
 
-    
-    // Scaffold is a basic application container that provides a lot of basic
-    // Application stuff in its members for free, like an AppBarr or a body.
-    return Scaffold(
-      body: Center(
+    Container portInputBox = createPortInput();
+    Row controlPanel = createControlPanel(viewModel);
+    Expanded outputDataBox = createOutputBox(viewModel);
+    SizedBox spacer = SizedBox(height: 16.0);
 
-        // Container and SizeBox are basic layout elements.
-        // Container gives more options. In this case I didn't use any and 
-        // Flutter keeps trying to get me to use the simpler SizeBox instead.
-        child: Container(
-          width: 350,
-
-          // Padding is like a SizeBox, but instead of specifying a size, I 
-          //define its layout by defining the space around `child`.
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-
-            // Another Widget from the Provider package.
-            // Lets me listen and react to changes in our ViewModel (the 
-            // Provider).
-            // Reads values from the Provider and rebuilds the UI when they
-            // change.
-            // Gives us the `builder` method so we can interact with the state.
-            child: Consumer<ListenerViewModel>(
-
-              // *context* - the current build context, lets us access other
-              //     widgets or data from the widget tree. We don't really
-              //     make use of that here.
-              // *viewModel* - an instance of ListenerViewModel - you'll see 
-              //     me use this to check the state before I do things here
-              //     like set the text and color on the connection indicator.
-              // *child* - a widget that doesn't need to be rebuilt (unused).
-              builder: (context, viewModel, child) {
-
-                Container portInputBox = createPortInput();
-                Row controlPanel = createControlPanel(viewModel);
-                Expanded outputDataBox = createOutputBox(viewModel);
-                SizedBox spacer = SizedBox(height: 16.0);
-
-                Column pageRootColumn = Column(
-                  // minimize the vertical space use
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    portInputBox,
-                    spacer,
-                    controlPanel,
-                    spacer,
-                    outputDataBox,
-                  ],
-                );
-
-                return pageRootColumn;
-
-              },
-            ),
-          ),
-        ),
-      ),
+    Column pageRootColumn = Column(
+      // minimize the vertical space use
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        portInputBox,
+        spacer,
+        controlPanel,
+        spacer,
+        outputDataBox,
+      ],
     );
+
+    return pageRootColumn;
   }
 }
